cc6d33c6adb392cfe6c4784e8a954747f153c77f
Victor Fernandez de Alba <sneridagh@gmail.com>
2021-02-20 17:54:33 +0100

    Fix tests

--- src/components/BlockRenderer/BlockRenderer.test.jsx
+++ src/components/BlockRenderer/BlockRenderer.test.jsx
@@ -3,10 +3,29 @@ import renderer from 'react-test-renderer';
 import BlockRenderer from './BlockRenderer';
 import { Provider } from 'react-intl-redux';
 import configureStore from 'redux-mock-store';
+import config from '@plone/volto/registry';
 
 const mockStore = configureStore();
 
-global.__SERVER__ = true; // eslint-disable-line no-underscore-dangle
+beforeAll(() => {
+  config.blocks.blocksConfig = {
+    ...config.blocks.blocksConfig,
+    image: {
+      id: 'image',
+      title: 'Image',
+      group: 'media',
+      view: () => <div className="mocked-block-view-image" />,
+      edit: () => <div className="mocked-block-edit-image" />,
+      restricted: false,
+      mostUsed: true,
+      sidebarTab: 1,
+      security: {
+        addPermission: [],
+        view: [],
+      },
+    },
+  };
+});
 
 test('renders a text block component using the block renderer', () => {
   const store = mockStore({
@@ -21,7 +40,7 @@ test('renders a text block component using the block renderer', () => {
         data={{ url: 'heroimage.jpg' }}
         block="theblockid"
         edit
-        type="text"
+        type="image"
         selected={false}
         onChangeBlock={() => {}}
       />
