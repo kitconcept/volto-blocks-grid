9a06fee5d43fff1f3c6de2b17f44ff9459dade00
Victor Fernandez de Alba <sneridagh@gmail.com>
2021-03-18 17:46:03 +0100

    Improved Teaser

--- src/components/Teaser/utils.js
+++ src/components/Teaser/utils.js
@@ -1,6 +1,12 @@
+import { isInternalURL } from '@plone/volto/helpers';
+
 export function getTeaserImageURL(href, image) {
   if (image) {
-    return `${image['@id']}/@@images/image/teaser`;
+    if (isInternalURL(image['@id'])) {
+      return `${image['@id']}/@@images/image/teaser`;
+    } else {
+      return image['@id'];
+    }
   } else {
     return `${href['@id']}/@@images/preview_image/teaser`;
   }
