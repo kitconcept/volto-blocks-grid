8066c192809c68bf2ee10110fa78de9b480c5440
Víctor Fernández de Alba <sneridagh@gmail.com>
2021-05-22 10:48:10 +0200

    Move to core components (#16)

--- src/components/Teaser/DefaultBody.jsx
+++ src/components/Teaser/DefaultBody.jsx
@@ -5,7 +5,7 @@ import { defineMessages, useIntl } from 'react-intl';
 import imageBlockSVG from '@plone/volto/components/manage/Blocks/Image/block-image.svg';
 import { flattenToAppURL } from '@plone/volto/helpers';
 import { getTeaserImageURL } from './utils';
-import { MaybeWrap } from '../../components';
+import { MaybeWrap } from '@plone/volto/components';
 import { UniversalLink } from '@plone/volto/components';
 
 const messages = defineMessages({
