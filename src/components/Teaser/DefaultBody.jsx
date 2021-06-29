e88162c71daef0e663e5fccf3ed432ecd54f8c00
iFlameing <ialokkumarsingh0@gmail.com>
2021-06-29 20:32:22 +0530

    minor fix

--- src/components/Teaser/DefaultBody.jsx
+++ src/components/Teaser/DefaultBody.jsx
@@ -50,7 +50,8 @@ const TeaserDefaultTemplate = (props) => {
               </div>
             )}
             <div className="content">
-              <h2>{data?.headtitle}</h2>
+              {data?.headtitle && <h2>{data?.headtitle}</h2>}
+
               <h3>{data?.title}</h3>
               {!data.hide_description && <p>{data?.description}</p>}
             </div>
