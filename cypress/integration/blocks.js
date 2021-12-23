bd8e5e1af12284a17ba2ea39901f5d8660bf1318
Victor Fernandez de Alba <sneridagh@gmail.com>
2021-12-23 17:25:28 +0100

    Fix Cypress now

--- cypress/integration/blocks.js
+++ cypress/integration/blocks.js
@@ -129,11 +129,16 @@ context('Blocks Acceptance Tests', () => {
         '.imagesGrid.two [data-rbd-draggable-context-id]:first-child .toolbar-inner .buttons:first-child button',
       ).click();
       cy.findByLabelText('Select My Image').dblclick();
+      cy.get(
+        '.imagesGrid.two [data-rbd-draggable-context-id]:first-child',
+      ).click();
+      cy.get('#field-alt').click().type('My Image');
 
       cy.get(
-        '.imagesGrid.two [data-rbd-draggable-context-id]:last-child .toolbar-inner .buttons:first-child button',
+        '.imagesGrid.two [data-rbd-draggable-context-id]:last-child',
       ).click();
       cy.findByLabelText('Select My Image').dblclick();
+      cy.get('#field-alt').click().type('My Image');
 
       cy.get('#toolbar-save').click();
 
