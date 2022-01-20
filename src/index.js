5d70bb1ae383956579611c67b4abd73b7434f82a
ionlizarazu <ilizarazu@codesyntax.com>
2022-01-20 12:17:30 +0100

    add maxNumberOfColumns configuration, 4 by default

--- src/index.js
+++ src/index.js
@@ -26,6 +26,7 @@ const customBlocks = {
       addPermission: [],
       view: [],
     },
+    maxNumberOfColumns: 4,
     gridAllowedBlocks: ['teaser', 'image', 'listing', 'slate', 'text'],
   },
   teaserGrid: {
