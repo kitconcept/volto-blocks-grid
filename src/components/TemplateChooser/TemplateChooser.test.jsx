d90f287eedbd03102fc5ae5ae12fd410745c369a
Victor Fernandez de Alba <sneridagh@gmail.com>
2022-06-08 17:08:13 +0200

    Fix tests

--- src/components/TemplateChooser/TemplateChooser.test.jsx
+++ src/components/TemplateChooser/TemplateChooser.test.jsx
@@ -4,8 +4,27 @@ import configureStore from 'redux-mock-store';
 import { Provider } from 'react-intl-redux';
 import TemplateChooser from './TemplateChooser';
 import templateSVG from './template.svg';
+import { v4 as uuid } from 'uuid';
+import config from '@plone/volto/registry';
 
 const mockStore = configureStore();
+
+beforeAll(() => {
+  config.blocks.blocksConfig = {
+    ...config.blocks.blocksConfig,
+    __grid: {
+      id: '__grid',
+      title: 'Grid',
+      group: 'media',
+      view: () => <div className="mocked-block-view-grid" />,
+      edit: () => <div className="mocked-block-edit-grid" />,
+      restricted: false,
+      mostUsed: true,
+      sidebarTab: 1,
+      maxNumberOfColumns: 4,
+    },
+  };
+});
 
 test('renders a TemplateChooser component', () => {
   const store = mockStore({
@@ -23,6 +42,12 @@ test('renders a TemplateChooser component', () => {
             image: templateSVG,
             id: 'templateid',
             defaultMessage: 'Template default translation',
+            columns: [
+              {
+                id: uuid(),
+                type: 'teaser',
+              },
+            ],
           },
         ]}
         onSelectTemplate={() => {}}
