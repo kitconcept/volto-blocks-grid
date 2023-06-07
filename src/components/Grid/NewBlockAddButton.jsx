c8e710673e6a1c5e84189ac52acfa8ee6ce5a1ab
Victor Fernandez de Alba <sneridagh@gmail.com>
2023-06-07 16:08:26 +0200

    Enable popperjs based blockschooser

--- src/components/Grid/NewBlockAddButton.jsx
+++ src/components/Grid/NewBlockAddButton.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { Button } from 'semantic-ui-react';
+import { Button, Ref } from 'semantic-ui-react';
 import { BlockChooser, Icon } from '@plone/volto/components';
 import useOutsideClick from '../../helpers/useOutsideClick/useOutsideClick';
+import { usePopper } from 'react-popper';
+import { Portal } from 'react-portal';
+
 import addSVG from '@plone/volto/icons/add.svg';
 
 const NewBlockAddButton = (props) => {
@@ -9,20 +12,31 @@ const NewBlockAddButton = (props) => {
   const ref = React.useRef();
   const [isOpenMenu, setOpenMenu] = React.useState(false);
 
+  const [referenceElement, setReferenceElement] = React.useState(null);
+  const [popperElement, setPopperElement] = React.useState(null);
+  const { styles, attributes } = usePopper(referenceElement, popperElement, {
+    placement: 'bottom',
+    modifiers: [
+      {
+        name: 'offset',
+        options: {
+          offset: [0, -50],
+        },
+      },
+      {
+        name: 'flip',
+        options: {
+          fallbackPlacements: ['right', 'top'],
+        },
+      },
+    ],
+  });
+
   useOutsideClick(ref, () => setOpenMenu(false));
 
   return (
     <>
-      {isOpenMenu ? (
-        <div ref={ref}>
-          <BlockChooser
-            onMutateBlock={(block, value) => onChangeGridItem(index, value)}
-            currentBlock={block}
-            showRestricted
-            allowedBlocks={allowedBlocks}
-          />
-        </div>
-      ) : (
+      <Ref innerRef={setReferenceElement}>
         <Button
           basic
           icon
@@ -32,7 +46,24 @@ const NewBlockAddButton = (props) => {
         >
           <Icon name={addSVG} className="circled" size="24px" />
         </Button>
-      )}
+      </Ref>
+      {isOpenMenu ? (
+        <Portal node={document.getElementById('body')}>
+          <div
+            ref={setPopperElement}
+            style={styles.popper}
+            {...attributes.popper}
+          >
+            <BlockChooser
+              onMutateBlock={(block, value) => onChangeGridItem(index, value)}
+              currentBlock={block}
+              showRestricted
+              allowedBlocks={allowedBlocks}
+              ref={ref}
+            />
+          </div>
+        </Portal>
+      ) : null}
     </>
   );
 };
